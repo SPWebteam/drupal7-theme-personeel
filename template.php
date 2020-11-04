58643da0ffd759245380eac601a45713c6f00a30
chriseikelboom <ceikelboom@sp.nl>
2020-11-04 09:57:05 +0100

    fix view title from not showing

--- template.php
+++ template.php
@@ -212,3 +212,11 @@ function spass_personeel_date_nav_title($params) {
     return $title;
   }
 }
+
+/**
+ * Render title from view into page.
+ **/
+function spass_personeel_preprocess_views_view(&$vars) {
+  $view = $vars['view'];
+  $vars['title'] = filter_xss_admin($view->get_title());
+}
